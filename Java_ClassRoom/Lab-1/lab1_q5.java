e037395cda817459ac35b04226bea95dfab7d669
sRtv <148667786+ShrutiVerma3008@users.noreply.github.com>
2024-11-22 20:27:36 +0530

    Update lab1_q5.java

--- Java_ClassRoom/Lab-1/lab1_q5.java
+++ Java_ClassRoom/Lab-1/lab1_q5.java
@@ -9,10 +9,11 @@
 // }
 public class lab1_q5 {
     public static void main(String[] args) {
-        // int[] array = new array(5);
-        
-
+     
+        System.out.printf("%-10s %-10s %-10s%n", "Number", "Square", "Cube");
+        System.out.println("---------------------------------------");
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("%-10d %-10d %-10d%n", i, i * i, i * i * i);
+        }
     }
-
-    
 }
