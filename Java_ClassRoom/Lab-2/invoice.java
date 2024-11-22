6dde8a0a8cfad25098bd56c03d16f1abffcc4a83
sRtv <148667786+ShrutiVerma3008@users.noreply.github.com>
2024-11-22 20:40:58 +0530

    Add files via upload

---
+++ Java_ClassRoom/Lab-2/invoice.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class info{
+    // String part_num;
+    // String desc;
+    // int quantity;
+    // double price_per;
+
+    int quantity()
+    
+
+}
+public class invoice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the part number : ");
+    part_num =sc.next();
+    System.out.println("Enter the part description : ");
+    desc =sc.next();
+    System.out.println("Enter the  quantity : ");
+    part_num =sc.nextInt();
+    }
+
+    
+}
